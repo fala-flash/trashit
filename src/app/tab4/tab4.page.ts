c16f8850574b8a744581a62c5a68329986ecefc8
fala-flash <44972556+fala-flash@users.noreply.github.com>
2020-04-06 16:16:46 +0200

    status page elements

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+const notifyString: string = "p0c1";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab4.page.html',
-  styleUrls: ['tab4.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab4.page.html",
+  styleUrls: ["tab4.page.scss"],
 })
-export class Tab4Page {
+export class Tab4Page implements OnInit {
+  plasticStatus: string = "";
+  paperStatus: string = "";
 
   constructor() {}
+  ngOnInit(): void {
+    this.getPlasticStatus();
+    this.getPaperStatus();
+  }
 
+  getPlasticStatus(): string {
+    if (notifyString[1] == "0") {
+      this.plasticStatus = "Empty";
+    } else if (notifyString[1] == "2") {
+      this.plasticStatus = "Full";
+    } else if (notifyString[1] == "1") {
+      this.plasticStatus = "50% Full";
+    } else this.plasticStatus = "Info not available";
+    return this.plasticStatus;
+  }
+
+  getPaperStatus(): string {
+    if (notifyString[3] == "0") {
+      this.paperStatus = "Empty";
+    } else if (notifyString[3] == "2") {
+      this.paperStatus = "Full";
+    } else if (notifyString[3] == "1") {
+      this.paperStatus = "50% Full";
+    } else this.paperStatus = "Info not available";
+    return this.paperStatus;
+  }
 }
