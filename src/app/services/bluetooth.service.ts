ca53cc66262c1f1ef2b91b54bf89d7518e337bea
fala-flash <44972556+fala-flash@users.noreply.github.com>
2020-04-18 11:13:07 +0200

    basketStatus

--- src/app/services/bluetooth.service.ts
+++ src/app/services/bluetooth.service.ts
@@ -1,115 +1,118 @@
-import { Tab4Page } from './../tab4/tab4.page';
-import { Injectable } from '@angular/core';
-import {  NgZone } from '@angular/core';
-import { BLE } from '@ionic-native/ble/ngx';
-import { ToastController } from '@ionic/angular';
+import { Injectable } from "@angular/core";
+import { NgZone } from "@angular/core";
+import { BLE } from "@ionic-native/ble/ngx";
+import { ToastController } from "@ionic/angular";
+import { BehaviorSubject, Observable } from "rxjs";
 
-
-const mac = '9C:1D:58:90:C2:AA';
-const serv = 'ffe0';
-const char = 'ffe1';
-
+const mac = "9C:1D:58:90:C2:AA";
+const serv = "ffe0";
+const char = "ffe1";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class BluetoothService {
-
-
-  devices:any[] = [];
+  devices: any[] = [];
 
   stringa: any[] = [4];
 
   num: number = 0;
 
-  constructor(private ble:BLE,private ngZone: NgZone, public toastController: ToastController, private status: Tab4Page) { }
+  private PAPERSTATUSSUBJECT = new BehaviorSubject<number>(0);
+  private PLASTICSTATUSSUBJECT = new BehaviorSubject<number>(0);
 
+  constructor(
+    private ble: BLE,
+    private ngZone: NgZone,
+    public toastController: ToastController
+  ) {}
 
-
-  enableBluetooth(){
+  enableBluetooth() {
     this.ble.enable();
   }
 
   async presentToastConnected(msg: string) {
     const toast = await this.toastController.create({
       message: msg,
-      duration: 2000
+      duration: 2000,
     });
     toast.present();
   }
 
-  
-
-  Scan(){
+  Scan() {
     this.devices = [];
-    this.ble.scan([],15).subscribe(
-      device => this.onDeviceDiscovered(device)
-    );
-      
+    this.ble
+      .scan([], 15)
+      .subscribe((device) => this.onDeviceDiscovered(device));
   }
-  onDeviceDiscovered(device){
-    console.log('Discovered' + JSON.stringify(device,null,2));
-    this.ngZone.run(()=>{
-      this.devices.push(device)
+  onDeviceDiscovered(device) {
+    console.log("Discovered" + JSON.stringify(device, null, 2));
+    this.ngZone.run(() => {
+      this.devices.push(device);
       if (device.id === mac) {
         this.connect();
       }
-    })
+    });
   }
 
-  connect(){
-    this.ble.connect(mac).subscribe(data => {
+  connect() {
+    this.ble.connect(mac).subscribe((data) => {
       this.OnConnectionSuccesfull(data);
     });
   }
 
-
   OnConnectionSuccesfull(data: any) {
     this.message();
-    this.ble.startNotification(data.id, serv, char).subscribe(res => {
-      this.onDataChanged(res);
-    }, err => this.presentToastConnected(err))
+    this.ble.startNotification(data.id, serv, char).subscribe(
+      (res) => {
+        this.onDataChanged(res);
+      },
+      (err) => this.presentToastConnected(err)
+    );
   }
-
 
   onDataChanged(res: ArrayBuffer) {
     var value = new Uint8Array(res);
     if (value[0] != 10 && this.num != 4) {
       this.stringa[this.num] = String.fromCharCode(value[0]);
       this.num++;
-    }
-    else if (this.num == 4 || value[0] == 10) {
+    } else if (this.num == 4 || value[0] == 10) {
       this.num = 0;
       this.ble.disconnect(mac).then(() => {
         this.presentToastConnected(this.generateString());
-        this.status.getPaperStatus(this.generateString());
-        this.status.getPlasticStatus(this.generateString());
-      });;
-      
+      });
     }
-
   }
 
-  generateString(): string{
-    var s: string = this.stringa.join('');
+  generateString(): string {
+    var s: string = this.stringa.join("");
     return s;
   }
 
-
-  sendMessage(){
-    this.ble.isConnected(mac).then(() => {
-      this.message();
-    }, () => {
-      this.Scan();
-    })  
+  sendMessage() {
+    this.ble.isConnected(mac).then(
+      () => {
+        this.message();
+      },
+      () => {
+        this.Scan();
+      }
+    );
   }
 
-  message(){
+  message() {
     var data = new Uint8Array(1);
     data[0] = 111;
     this.ble.write(mac, serv, char, data.buffer).then(() => {
-      console.log('hello arduino');
-    })
+      console.log("hello arduino");
+    });
   }
 
+  public getPaperStatus(): Observable<number> {
+    return this.PAPERSTATUSSUBJECT.asObservable();
+  }
+
+  public getPlasticStatus(): Observable<number> {
+    return this.PLASTICSTATUSSUBJECT.asObservable();
+  }
 }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,42 +1,62 @@
-import { Component, OnInit, Injectable } from "@angular/core";
-
-
+import { BluetoothService } from "./../services/bluetooth.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-tab2",
   templateUrl: "tab4.page.html",
   styleUrls: ["tab4.page.scss"],
 })
-@Injectable({
-  providedIn: 'root'
-})
-
 export class Tab4Page implements OnInit {
   plasticStatus: string = "";
   paperStatus: string = "";
 
-  constructor() {}
-  ngOnInit(): void {  }
+  constructor(private bleService: BluetoothService) {
+    this.getPaperStatus();
+    this.getPlasticStatus();
+  }
+  ngOnInit(): void {}
 
-  getPlasticStatus(notifyString): string {
-    if (notifyString[1] == "0") {
-      this.plasticStatus = "Empty";
-    } else if (notifyString[1] == "2") {
-      this.plasticStatus = "Full";
-    } else if (notifyString[1] == "1") {
-      this.plasticStatus = "50% Full";
-    } else this.plasticStatus = "Info not available";
-    return this.plasticStatus;
+  getPaperStatus() {
+    this.bleService.getPaperStatus().subscribe((status) => {
+      switch (status) {
+        case 0:
+          this.paperStatus = "Empty";
+          break;
+
+        case 1:
+          this.paperStatus = "50% Full";
+          break;
+
+        case 2:
+          this.paperStatus = "Full";
+          break;
+
+        default: 
+        this.paperStatus = 'Info not available';
+        break;
+      }
+    });
   }
 
-  getPaperStatus(notifyString): string {
-    if (notifyString[3] == "0") {
-      this.paperStatus = "Empty";
-    } else if (notifyString[3] == "2") {
-      this.paperStatus = "Full";
-    } else if (notifyString[3] == "1") {
-      this.paperStatus = "50% Full";
-    } else this.paperStatus = "Info not available";
-    return this.paperStatus;
+  getPlasticStatus() {
+    this.bleService.getPlasticStatus().subscribe((status) => {
+      switch (status) {
+        case 0:
+          this.plasticStatus = "Empty";
+          break;
+
+        case 1:
+          this.plasticStatus = "50% Full";
+          break;
+
+        case 2:
+          this.plasticStatus = "Full";
+          break;
+
+        default: 
+        this.plasticStatus = 'Info not available';
+        break;
+      }
+    });
   }
 }
